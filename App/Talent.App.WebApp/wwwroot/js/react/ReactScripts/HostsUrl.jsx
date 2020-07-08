a10ff2cd5cb9061113205d3561ec09bf5a2f4ff9
maryrose <maryrose.legasp2018@gmail.com>
2020-07-08 23:01:05 +1000

    fixing controller for filter

--- App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/HostsUrl.jsx
+++ App/Talent.App.WebApp/wwwroot/js/react/ReactScripts/HostsUrl.jsx
@@ -1,18 +1,18 @@
 ﻿
 // //Development url
-export const TALENT_APP_WEBAPP = 'http://localhost:61771';
-export const TALENT_API = 'http://localhost:60880';
-export const TALENT_SERVICES_IDENTITY = 'http://localhost:60998';
-export const TALENT_SERVICES_PROFILE = 'http://localhost:60290';
-export const TALENT_SERVICES_TALENT = 'http://localhost:51689';
+//export const TALENT_APP_WEBAPP = 'http://localhost:61771';
+//export const TALENT_API = 'http://localhost:60880';
+//export const TALENT_SERVICES_IDENTITY = 'http://localhost:60998';
+//export const TALENT_SERVICES_PROFILE = 'http://localhost:60290';
+//export const TALENT_SERVICES_TALENT = 'http://localhost:51689';
 
 
 
 
 // Production url
- //export const TALENT_APP_WEBAPP = 'https://talentappwebappmvp.azurewebsites.net';
- //export const TALENT_API = 'https://talentapimvp.azurewebsites.net';
- //export const TALENT_SERVICES_IDENTITY = 'https://talentservicesidentitymvp.azurewebsites.net';
- //export const TALENT_SERVICES_PROFILE = 'https://talentservicesprofilemvp.azurewebsites.net';
- //export const TALENT_SERVICES_TALENT = 'https://talentservicestalentmvp.azurewebsites.net';
+ export const TALENT_APP_WEBAPP = 'https://talentappwebappmvp.azurewebsites.net';
+ export const TALENT_API = 'https://talentapimvp.azurewebsites.net';
+ export const TALENT_SERVICES_IDENTITY = 'https://talentservicesidentitymvp.azurewebsites.net';
+ export const TALENT_SERVICES_PROFILE = 'https://talentservicesprofilemvp.azurewebsites.net';
+ export const TALENT_SERVICES_TALENT = 'https://talentservicestalentmvp.azurewebsites.net';
 
